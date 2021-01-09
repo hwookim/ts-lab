eda02d727db2ff9ac15ecc01b5b3afcb463dc679
hwookim <gusdn9754@gmail.com>
2021-01-09 17:15:24 +0900

    chore: eslint, prettier 설정

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -2,8 +2,8 @@ import { sum } from "../src/sum";
 
 describe("테스트 작성해보기", () => {
   it("sum", () => {
-    const a: number = 10;
-    const b: number = 25;
+    const a = 10;
+    const b = 25;
     const result: number = sum(a, b);
 
     expect(result).toEqual(a + b);
